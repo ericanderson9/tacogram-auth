6fb1ac939007c28ddad1a0ddc2d328c4d96ed5d6
Eric Anderson <eric.anderson1@kellogg.northwestern.edu>
2024-05-23 18:25:20 +0000

    attemp2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,14 @@ class SessionsController < ApplicationController
   
   def create
     # TODO: authenticate user
+    if User.find_by({ "email" => params["email"] }) == nil
+    
+    elsif @user["password"] = params["password"]
+      flash["notice"] = "Login Successful"
+      redirect_to "/posts"
+    else
     flash["notice"] = "Nope."
     redirect_to "/login"
+    end
   end
 end
